ad0c8919950db399433ef3f75c40b19f7a079fe1
javierjoyera <javi.joyera@gmail.com>
2024-01-07 21:51:22 +0100

    Introduccion realizada

--- 04-introduccion-flask/01-HolaMundo/app.py
+++ 04-introduccion-flask/01-HolaMundo/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -18,3 +18,21 @@ def hello(nombre):
 def suma(num1, num2):
     return f'<p>La suma es {num1 + num2}</p>'
 
+@app.route('/mostrar/<nombre>', methods=['GET', 'POST'])
+def mostrar_nombre(nombre):
+    return render_template('mostrar.html', nombre=nombre)
+
+@app.route("/redirect")
+def redirect_index():
+    return redirect(url_for('index'))
+
+@app.errorhandler(404)
+def page_not_found():
+    return render_template('404.html', error='Página no encontrada...'), 404
+
+@app.route('/login/<user>/<password>')
+def login(user, password):
+    if user == 'admin' and password == 'admin':
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('page_not_found', error='Usuario o contraseña incorrectos'))
